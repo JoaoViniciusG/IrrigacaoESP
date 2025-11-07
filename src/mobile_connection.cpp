cc4e1db04a6bf38d7056df89a08242305ed56b14
JoaoGuerini <joaopedrogp83@gmail.com>
2025-11-07 11:38:33 -0400

    Att

--- src/mobile_connection.cpp
+++ src/mobile_connection.cpp
@@ -75,25 +75,21 @@ void awaitingMobileConnection()
           String pass = doc["pass"] | "";
 
           // FEEDBACK: CONECTANDO AO WIFI
-          JsonDocument prog; prog["type"]="provision_progress"; prog["stage"]="Conectando-se a rede Wifi...";
+          JsonDocument prog; prog["type"]="provision_progress"; prog["stage"]=0;
           String o; serializeJson(prog, o); ws.sendTXT(num, o);
 
           String reason;
           bool wifiOk = tryConnectWifi(ssid, pass, reason);
           if (!wifiOk) {
-            JsonDocument res; res["type"]="provision_result"; res["ok"]=false; res["reason"]=reason;
+            JsonDocument res; res["type"]="provision_result"; res["stage"]=0; res["ok"]=false;
             String oo; serializeJson(res, oo); ws.sendTXT(num, oo);
 
             sendNetworks(num);
             return;
           }
 
-          // FEEDBACK: IP LOCAL
-          prog.clear(); prog["type"]="provision_progress"; prog["stage"]="Obtendo IP local.";
-          o.clear(); serializeJson(prog, o); ws.sendTXT(num, o);
-
           // FEEDBACK: CONECTANDO COM O SERVIDOR
-          prog.clear(); prog["type"]="provision_progress"; prog["stage"]="Conectando-se aos servidores.";
+          prog.clear(); prog["type"]="provision_result"; prog["ok"]=true; prog["stage"]=0;
           o.clear(); serializeJson(prog, o); ws.sendTXT(num, o);
 
           // ENVIANDO DADOS
@@ -102,7 +98,7 @@ void awaitingMobileConnection()
 
           // FEEDBACK: FALHA NA CONEXÃO
           if (!apiOk) {
-            JsonDocument res; res["type"]="provision_result"; res["ok"]=false; res["reason"]="Falha na conexão!";
+            JsonDocument res; res["type"]="provision_result"; res["stage"]=1; res["ok"]=false;
             String oo; serializeJson(res, oo); ws.sendTXT(num, oo);
             return;
           }
@@ -111,11 +107,11 @@ void awaitingMobileConnection()
           loadConfig();
 
           // FEEDBACK: SUCESSO
-          JsonDocument res; res["type"]="provision_result"; res["ok"]=true;
+          JsonDocument res; res["type"]="final";
           String oo; serializeJson(res, oo); ws.sendTXT(num, oo);
 
           // RECEBE OS ÚLTIMOS PACOTES
-          delay(300);
+          delay(3000);
 
           // FECHA O WEBSOCKET E O AP
           Serial.println("Conexão AP desconectada!");
